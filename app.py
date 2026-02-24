41ab91cc19a55f121d6ee48fb1e2912f6c589fa8
Maria Clara <mariaclara.dg25@gmail.com>
2026-02-24 12:44:49 -0300

    FastAPI

--- app.py
+++ app.py
@@ -24,8 +24,6 @@ if response.status_code == 200:
 else:
     print(f'O erro foi {response.status_code}')
     
-# print(dados_restaurante['McDonald’s'])
-
 for nome_do_restaurante, dados in dados_restaurante.items():
     nome_do_arquivo = f'{nome_do_restaurante}.json'
     with open(nome_do_arquivo, 'w') as arquivo_restaurante:
